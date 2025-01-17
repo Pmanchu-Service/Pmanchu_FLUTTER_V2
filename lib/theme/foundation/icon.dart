cc05a18d8f27daa18dc21b36833aa9caf48baf96
b_g <b_g@dsm.hs.kr>
2025-01-18 02:36:14 +0900

    fix: AssetIcons provide instance getter

--- lib/theme/foundation/icon.dart
+++ lib/theme/foundation/icon.dart
@@ -4,25 +4,25 @@ import 'package:flutter_svg/flutter_svg.dart';
 const _root = 'assets/icons';
 
 class AssetIcons {
-  static String get back => '$_root/back.svg';
-  static String get bell => '$_root/bell.svg';
-  static String get camera => '$_root/camera.svg';
-  static String get cancel => '$_root/cancel.svg';
-  static String get check => '$_root/check.svg';
-  static String get down => '$_root/down.svg';
-  static String get edit => '$_root/edit.svg';
-  static String get heart => '$_root/heart.svg';
-  static String get heartOutline => '$_root/heart_outline.svg';
-  static String get home => '$_root/home.svg';
-  static String get link => '$_root/link.svg';
-  static String get logo => '$_root/logo.svg';
-  static String get logoWithText => '$_root/logo_with_text.svg';
-  static String get plus => '$_root/plus.svg';
-  static String get required => '$_root/required.svg';
-  static String get settings => '$_root/settings.svg';
-  static String get settingsLine => '$_root/settings_line.svg';
-  static String get user => '$_root/user.svg';
-  static String get userSearch => '$_root/user_search.svg';
+  String get back => '$_root/back.svg';
+  String get bell => '$_root/bell.svg';
+  String get camera => '$_root/camera.svg';
+  String get cancel => '$_root/cancel.svg';
+  String get check => '$_root/check.svg';
+  String get down => '$_root/down.svg';
+  String get edit => '$_root/edit.svg';
+  String get heart => '$_root/heart.svg';
+  String get heartOutline => '$_root/heart_outline.svg';
+  String get home => '$_root/home.svg';
+  String get link => '$_root/link.svg';
+  String get logo => '$_root/logo.svg';
+  String get logoWithText => '$_root/logo_with_text.svg';
+  String get plus => '$_root/plus.svg';
+  String get required => '$_root/required.svg';
+  String get settings => '$_root/settings.svg';
+  String get settingsLine => '$_root/settings_line.svg';
+  String get user => '$_root/user.svg';
+  String get userSearch => '$_root/user_search.svg';
 }
 
 class AssetIcon extends StatelessWidget {
