c3d05b2fd4caaf5591f878f7b4430dd7bd30ffda
b_g <b_g@dsm.hs.kr>
2025-01-20 22:47:27 +0900

    feat: create PostHeader widget

---
+++ lib/theme/component/post_header.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:pmanchu_v2/service/theme_service.dart';
+import 'package:pmanchu_v2/theme/foundation/icon.dart';
+
+class PostHeader extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback? onSettingPressed;
+  const PostHeader({super.key, this.onSettingPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      leading: IconButton(
+        onPressed: Navigator.of(context).pop,
+        icon: AssetIcon(path: context.theme.icons.back)
+      ),
+      actions: onSettingPressed == null
+        ? null
+        : [
+          IconButton(
+            onPressed: onSettingPressed,
+            icon: AssetIcon(path: context.theme.icons.settings)
+          )
+      ]
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(45);
+}
